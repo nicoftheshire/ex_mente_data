f92fc7fffbc9a04a24fe011120e7500c4180def4
Holtzhausen, NM, Mnr [22639969@sun.ac.za] <22639969@sun.ac.za>
2024-02-01 10:54:25 +0200

    INITIAL COMMIT

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Ex Mente Junior Developer Assignment'
+
+setup (
+	name='DataAnalysis',
+	version=VERSION,
+	author='Nicholas Holtzhausen',
+	author_email='nholtzhausen54@gmail.com',
+	description=DESCRIPTION,
+	packages=find_packages,
+	install_requires=['pandas', 'matplotlib'],
+	keywords=['python', 'data', 'analysis']
+)
