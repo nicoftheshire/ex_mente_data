6841d6cf7cb642e96f822292d738556d97e633df
Holtzhausen, NM, Mnr [22639969@sun.ac.za] <22639969@sun.ac.za>
2024-02-07 09:04:43 +0200

    BASIC DATA ANALYSIS DONE The function (basic_analysis) that calculates basic summary and descriptive statistics is complete.

--- DataAnalysis/app.py
+++ DataAnalysis/app.py
@@ -3,7 +3,7 @@ This module implements the main data analysis and visualisation functionality.
 
 Author: Nicholas Holtzhausen
 """
-
+import pandas
 import pandas as pd
 import matplotlib as plt
 
@@ -47,7 +47,7 @@ def df_to_csv(df):
 	return file
 
 
-def basic_analysis(df):
+def basic_analysis(df, col, df_type='cat'):
 	"""
 	Reads a pandas dataframe and calculates basic summary and descriptive statistics,
 	and provides some basic data visualisation.
@@ -57,6 +57,38 @@ def basic_analysis(df):
 
 	df : pandas.DataFrame
 		A dataframe containing the data
+	col : str
+		The column for which statistics must be calculated
+	df_type : str
+		The type of df being passed to the function.
+		Options:
+			'cat' (default): a df with categorical columns
+			'num': a df with numerical columns
 	"""
+
+	result_data = {}
+	if df_type == 'num':
+		mean = df[col].mean()
+		median = df[col].median()
+		std = df[col].std()
+
+		result_data = {
+			'Mean': mean,
+			'Median': median,
+			'Standard Deviation': std
+		}
+
+	elif df_type == 'cat':
+		count = df[col].count()
+		unique = df[col].unique()
+
+		result_data = {
+			'Count': count,
+			'Unique': unique
+		}
+
+	return result_data
+
+
 
 
